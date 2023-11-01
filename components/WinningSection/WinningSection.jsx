23b69b8d6140d92041a49a7b1bece7a38df2e965
vandilsonbrito <vandilson25@outlook.com>
2023-11-01 18:22:43 -0300

    Create WinningSection component

---
+++ components/WinningSection/WinningSection.jsx
@@ -0,0 +1,8 @@
+
+export default function WinningSection() {
+  return (
+    <div className='w-full h-screen bg-[#2b2da1] p-5'>
+        <h2 className="text-[#000000] font-Montserrat ">Por que me acompanhar?</h2>
+    </div>
+  )
+}
