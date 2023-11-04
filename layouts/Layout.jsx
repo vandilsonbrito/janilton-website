73962db9d6ad2749e8dc727edc159dc1ec18ad26
vandilsonbrito <vandilson25@outlook.com>
2023-11-04 11:00:30 -0300

    Updating Layout and removing Outlet

--- layouts/Layout.jsx
+++ layouts/Layout.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router-dom";
+
 import Header from "../components/Header/Header";
+import Banner from "../components/Banner/Banner";
+import Convencimento from "../components/WinningSection/WinningSection";
+import Sobre from '../components/AboutSection/AboutSection';
+import Canal from '../components/ChannelSection/ChannelSection';
+import Contato from '../components/ContactSection/ContactSection';
 import Footer from "../components/Footer/Footer";
 
 export default function Layout() {
   return (
     <div>
         <Header/>
-        <Outlet/>
+        <Banner/>
+        <Convencimento/>
+        <Sobre/>
+        <Canal/>
+        <Contato/>
         <Footer/>
     </div>
   )
