389ecfdce92f8eaffc5fe46a77ab95745ef1f5ed
vandilsonbrito <vandilson25@outlook.com>
2023-11-15 17:45:44 -0300

    Update styles

--- src/components/ChannelSection/ChannelSection.jsx
+++ src/components/ChannelSection/ChannelSection.jsx
@@ -3,10 +3,10 @@ import { GrYoutube } from 'react-icons/gr';
 export default function CanalSection() {
   return (
     <div className="w-full h-full bg-main-blue text-white font-Montserrat px-8 py-14 lg:py-20 flex flex-col items-center" >
-      <h3 className="uppercase font-bold text-lg md:text-3xl" id='/Canal'>Meu canal do YouTube</h3>
-      <iframe src="https://www.youtube.com/watch?v=UgIwjLg4ONk" title='Canal do YouTube do Professor Janilton Almeida' className="w-[350px] h-[220px] sm:w-[480px] sm:h-[320px] md:w-[520px] md:h-[360px] lg:w-[650px] lg:h-[380px]  mt-8 md:mt-12 border-[8px] border-white"></iframe>
-      <a href="#" >
-        <button className="mt-10 w-fit flex items-center gap-3 h-[50px] bg-blue-600 text-white p-6 rounded-3xl font-semibold uppercase text-[1.05rem] border-[2px] border-blue-600 hover:border-white duration-200 ease-in-out active:scale-[0.98]">Conheça meu canal <GrYoutube className='mb-1 text-lg'/></button>
+      <h3 className="uppercase font-bold text-lg md:text-3xl" id='/canal'>Meu canal do YouTube</h3>
+      <iframe src="" title='Canal do YouTube do Professor Janilton Almeida' className="w-[350px] h-[220px] sm:w-[480px] sm:h-[320px] md:w-[520px] md:h-[360px] lg:w-[650px] lg:h-[380px]  mt-8 md:mt-12 border-[8px] border-white bg-slate-300"></iframe>
+      <a href="https://www.youtube.com/@prof.janiltonalmeida8089" target="_blank" rel="noopener noreferrer" className='mt-6'>
+        <button className="w-fit flex items-center gap-3 h-[50px] bg-blue-600 text-white p-6 rounded-3xl font-semibold uppercase text-[1.05rem] border-[2px] border-blue-600 hover:border-white duration-200 ease-in-out active:scale-[0.98]">Conheça meu canal <GrYoutube className='mb-1 text-lg'/></button>
       </a>
     </div>
   )
