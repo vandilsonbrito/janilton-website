e91dba1d473d4260c0921d24b9dd5d18bc0052f8
vandilsonbrito <vandilson25@outlook.com>
2023-11-15 17:45:04 -0300

    Update images and styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,25 +3,32 @@ import Wave from '../../assets/imgs-design/wave.svg'
 export default function Banner() {
   return (
     <div className='relative overflow-hidden'>
-        <div className="w-full h-[680px] overflow-hidden relative">
-            <div className={`w-full h-full ${window.innerWidth > 800 ? 'bg-Banner' : 'bg-BannerMobile'} bg-no-repeat bg-cover bg-right-bottom opacity-70 relative`}>
-                <div className="w-full h-full bg-[#3248916b] opacity- absolute"></div>
+        <div className="w-full h-[700px] md:h-[680px] overflow-hidden relative">
+            <div className={`w-full h-full ${window.innerWidth > 800 ? 'bg-Banner' : 'bg-BannerMobile'}  bg-no-repeat bg-cover bg-right-bottom opacity-70 relative`}>
+                
+                <div className="w-full h-full bg-[#8ba0e6a2] opacity- absolute"></div>
             </div>
             
             <div className="w-[100%] h-full flex flex-col lg:flex-row justify-center z-50 absolute top-24 pl-[0%] xl:pl-[12%] gap-0">
-                <div className="w-full h-fit flex flex-col items-center">
-                    <div className="w-full mt-[5%] px-10 flex flex-col  lg:px-0 lg:block lg:flex-row lg:mt-[10%]">
-                        <div className="w-full flex flex-col items-center -ml-6 sm:gap-5 lg:gap-0 xl:gap-3">
-                            <h2 className="text-5xl sm:text-7xl lg:leading-[7.5rem] lg:text-[4.5rem] xl:text-[6rem]  text-[#000]">Professor</h2>
-                            <h2 className="text-[3.3rem] sm:text-7xl lg:leading-[7.5rem] lg:text-[6rem] xl:text-[7rem] text-[#000000] lg:-mt-3 ml-20 lg:ml-32"><strong>Janilton <span className="ml-4 xl:ml-7">Almeida</span></strong></h2>
+                <div className="w-full h-fit flex flex-col items-center justify-center">
+                    <div className="w-[90%] mt-[2%]  px-10 flex flex-col lg:px-0 lg:block lg:flex-row lg:mt-[10%] lg:ml-3 xl:ml-0 2xl:mt-[5%]">
+                        <div className="w-full flex flex-col justify-center items-center sm:gap-2 lg:gap-0 xl:gap-3 text-black">
+                            
+                            <h2 className="text-5xl mt-2 sm:text-7xl lg:leading-[7.5rem] lg:text-[5rem] xl:text-[6rem]">Professor</h2>
+                        
+                            <h2 className="text-[3.3rem] sm:text-7xl lg:leading-[7.5rem] lg:text-[5.5rem] xl:text-[7rem] font-bold">Janilton</h2>
+                        
+                            <h2 className="text-[3.3rem] -mt-3 sm:text-7xl lg:leading-[7.5rem] lg:text-[5.5rem] xl:text-[7rem] font-bold">Almeida</h2>
+                           
                         </div>
+                        <div className="w-full text-center mt-2 lg:mt-0">
+                        <h1 className="text-[0.9rem] lg:text-xl bg-[#fff5f8] p-1">Domine a arte de dar aulas com um especialista em aulas criativas, pesquisa educacional e inovação.</h1>
                     </div>
-                    <div className="w-[65%] sm:w-[60%] lg:w-[95%] text-center mt-2 lg:mt-0">
-                        <h1 className="text-[0.9rem] lg:text-xl bg-[#fff5f8] lg:ml-[17%] xl:ml-[10%] mt-2 p-1">Domine a arte de dar aulas com um especialista em aulas criativas, pesquisa educacional e inovação.</h1>
                     </div>
+                    
                 </div>
                 <div className="w-[100%] h-full flex flex-col lg:flex-row items-center lg:px-[50px] xl:px-[140px]">
-                    <div className={`w-[300px] h-[350px] mt-5 sm:mt-0 lg:w-[450px] lg:h-[570px] ${window.innerWidth > 800 ? 'bg-Picture-Profile' : 'bg-Picture-Profile-Mobile'} bg-Picture-Profile bg-no-repeat bg-contain lg:-mt-16`}></div>
+                    <div className={`w-[300px] h-[350px]  lg:w-[450px] lg:h-[570px] ${window.innerWidth > 800 ? 'bg-Picture-Profile' : 'bg-Picture-Profile-Mobile'} bg-Picture-Profile bg-no-repeat bg-contain -mt-5 lg:-mt-16`}></div>
                 </div>
             </div>
         </div>
