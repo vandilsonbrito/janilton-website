3cf634971ec13057d27462dceb08a7c5aa2c8f0c
vandilsonbrito <vandilson25@outlook.com>
2023-11-15 17:37:51 -0300

    Update config. files

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,13 @@ export default {
     extend: {
       backgroundImage: {
         'BannerMobile': "url('../src/assets/imgs-design/relevo-fluido-mobile.webp')",
-        'Banner': "url('../src/assets/imgs-design/relevo-fluido.jpeg')",
-        'Picture-Profile': "url('../src/assets/imgs-janilton/foto-header.png')",
-        'Picture-Profile-Mobile': "url('../src/assets/imgs-janilton/foto-header.webp')"
+        'Banner': "url('../src/assets/imgs-design/relevo-fluido.webp')",
+        'Picture-Profile': "url('../src/assets/imgs-janilton/foto-header.webp')",
+        'Picture-Profile-Mobile': "url('../src/assets/imgs-janilton/foto-header-mobile.webp')"
       },
       fontFamily: {
-        Roboto: ['Roboto, sans-serif'],
-        Montserrat: ['Montserrat, sans-serif']
+        Montserrat: ['Montserrat, sans-serif'],
+        
       },
       colors: {
         'main-blue': '#2b2da1'
