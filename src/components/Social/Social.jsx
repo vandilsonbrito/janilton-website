915d973d7c44006046ce3cb360a36ff7c091d7e8
vandilsonbrito <vandilson25@outlook.com>
2023-11-15 18:07:16 -0300

    Update styles

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -4,10 +4,10 @@ import { GrYoutube, GrInstagram } from 'react-icons/gr';
 export default function Social() {
   return (
     <div className="w-fit mt-4 flex justify-center gap-5">
-        <a href="https://www.instagram.com/profjaniltonalmeidaeduc/" target="_blank" rel="noopener noreferrer" className="w-fit h-fit p-2 md:p-3 text-xl md:text-2xl text-blue-700 bg-white hover:bg-[#e44ca0] hover:text-white rounded-full cursor-pointer ease-in-out">
+        <a href="https://www.instagram.com/profjaniltonalmeidaeduc/" target="_blank" rel="noopener noreferrer" className="w-fit h-fit p-2 md:p-3 text-xl md:text-2xl text-blue-700 bg-white hover:bg-[#e44ca0] hover:text-white rounded-full cursor-pointer ease-in-out" aria-label="Siga o Professor Janilton Almeida no Instagram">
             <GrInstagram />
         </a>
-        <a href="#" target="_blank" rel="noopener noreferrer" className="w-fit h-fit p-2 md:p-3 text-xl md:text-2xl text-blue-700 bg-white hover:bg-red-500 hover:text-white rounded-full cursor-pointer ease-in-out">
+        <a href="https://www.youtube.com/@prof.janiltonalmeida8089" target="_blank" rel="noopener noreferrer" className="w-fit h-fit p-2 md:p-3 text-xl md:text-2xl text-blue-700 bg-white hover:bg-red-500 hover:text-white rounded-full cursor-pointer ease-in-out" aria-label="Inscreva-se no canal do Professor Janilton Almeida no YouTube">
             <GrYoutube />
         </a>
     </div>
