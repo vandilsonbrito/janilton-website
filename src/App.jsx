7092cb922a498f4861e441096d79d6b79b2f452e
vandilsonbrito <vandilson25@outlook.com>
2023-11-15 17:40:50 -0300

    Update Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Sobre from './components/AboutSection/AboutSection';
-import Login from "./pages/Login/Login";
+import DisplayArtigos from './pages/Artigos/MenuArtigos/MenuArtigos';
+import Artigos from './pages/Artigos/Artigos/Artigos';
+import MaisSobre from './pages/Sobre/Sobre';
+import CanalSection from './components/ChannelSection/ChannelSection';
+import ContactSection from './components/ContactSection/ContactSection';
+import SobrePage from './pages/Sobre/Sobre';
 
 function App() {
 
@@ -9,11 +14,17 @@ function App() {
     <>
         <Routes>
           <Route index element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/sobre' element={<SobrePage/>}/>
+          <Route path='/mais-sobre' element={<MaisSobre/>}/>
+          <Route path='/artigos' element={<DisplayArtigos/>}/>
+          <Route path='/artigos/:id' element={<Artigos articleId=''/>}/>
           <Route path='/Sobre' element={<Sobre/>}/>
-          <Route path='/Login' element={<Login/>}/>
-        </Routes> 
+          <Route path='/Canal' element={<CanalSection/>}/>
+          <Route path='/Contato' element={<ContactSection/>}/>
+        </Routes>   
     </>
   )
-}
+} 
 
 export default App
