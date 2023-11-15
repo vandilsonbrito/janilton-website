e757257f2b68f070f3e5ef32c9bbefd613208933
vandilsonbrito <vandilson25@outlook.com>
2023-11-15 17:49:07 -0300

    Update styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+
 import Social from '../Social/Social';
 
 export default function Footer() {
@@ -8,8 +8,8 @@ export default function Footer() {
             <h3 className="text-lg md:text-3xl font-semibold text-white lg:mt-2">Me siga nas redes socias.</h3>
             <Social/>
         </div>
-        <div className="w-full bg-blue-700 pb-2 pt-1 flex justify-center text-center px-3">
-          <p className='text-white'>Todos os direitos reservados ao Professor <Link to='/Login'>Janilton</Link>. Desenvolvido por <a className='underline' href="https://www.linkedin.com/in/vandilson-brito-b791b3216/">Vandilson Brito</a></p>
+        <div className="w-full bg-blue-700 pb-2 pt-1 flex justify-center text-center px-3 text-sm lg:text-base">
+          <p className='text-white'>Todos os direitos reservados ao Professor Janilton. Desenvolvido por <a className='underline' href="https://www.linkedin.com/in/vandilson-brito-b791b3216/">Vandilson Brito</a></p>
         </div>
     </div>
   )
