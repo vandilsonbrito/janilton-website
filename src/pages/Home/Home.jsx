dc85d5fe0da32a0e0d632dc0095557b35b3075a5
vandilsonbrito <vandilson25@outlook.com>
2023-11-16 15:19:25 -0300

    Add meta tag page description

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import Layout from "../../layouts/Layout";
+import { Helmet } from 'react-helmet-async';
 
 export default function Home() {
   return (
     <div>
+        <Helmet>
+          <meta name="description" content="O Professor Janilton é um especialista na arte de dar aulas criativas, pesquisa educacional e inovação. É um professor de Geografia com mais de vinte e cinco anos de atuação com Mestrado em Ensino de Ciências e Doutorado em Ciências. Ele possui um canal no YouTube onde compartilha seu conhecimento para ajudar alunos (com conteúdos de Geografia) e professores (para ajudá-los na elaboração de aulas criativas). Além disso, ele escreve artigos de opinião sobre diversos temas da área acadêmica"/>
+        </Helmet>
         <Layout/>
     </div>
   )
