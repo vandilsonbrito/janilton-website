3018945fbff057be41a90196e73c71b9e19d9f46
vandilsonbrito <vandilson25@outlook.com>
2023-11-16 15:18:57 -0300

    Create MenuArtigos page to show a menu of articles

---
+++ src/pages/Artigos/MenuArtigos/MenuArtigos.jsx
@@ -0,0 +1,71 @@
+import { useEffect } from 'react';
+import Footer from '../../../components/Footer/Footer';
+import HeaderOutter from '../../../components/Header/HeaderOutter';
+import SkeletonArticle from '../../../components/Loading/SkeletonArticle';
+import { handleDocument } from '../../../helpers/prismic/article';
+import DisplayArtigos from '../DisplayArtigos';
+import { motion } from 'framer-motion';
+import { Helmet } from 'react-helmet-async';""
+
+export default function MenuArtigos() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        document.body.style.overflow = 'auto';
+      }, []);
+
+    const { titles, descriptions, textContents, imagesurl, slugs } = handleDocument();
+    
+    const showArticles = (titles, descriptions, textContents, imagesurl, slugs) => {
+        const artigosDisplays = [];
+        for (let i = 0; i < titles.length; i++) {
+            artigosDisplays.push(<DisplayArtigos key={i} title={titles[i]} description={descriptions[i]} textContent={textContents[i]} imageurl={imagesurl[i]} slug={slugs[i]} />);
+        }
+        console.log('Menu de Artigos carregados!')
+        return artigosDisplays;
+    }
+
+    const fadeInOut = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+      };
+
+    return (
+        <>
+            <Helmet>
+                <meta name="description" content="Leia artigos de opinião sobre o ensino de Ciências, Geografia e área afins"/>
+            </Helmet>
+            <motion.div 
+                initial="hidden"
+                animate={'visible'}
+                variants={fadeInOut}
+                transition={{ duration: 0.3 }}
+                className='w-full h-[50%]'
+                >
+                    
+                {<HeaderOutter/>}
+                <div className='w-full h-full bg-white text-white py-28 lg:py-40 flex flex-col justify-center'>
+                    <div className="w-full flex justify-center pb-10 md:pb-20">
+                        <h1 className='text-3xl md:text-4xl text-black uppercase font-semibold'>Artigos</h1>
+                    </div>
+                    <div className="w-full h-full flex flex-wrap justify-center items-center gap-8 xl:gap-14">
+                        { (titles && descriptions && textContents && imagesurl && slugs) ? 
+                            showArticles(titles, descriptions, textContents, imagesurl, slugs) 
+                            :
+                                <>
+                                    <SkeletonArticle/>
+                                    <SkeletonArticle/>
+                                    <SkeletonArticle/>
+                                    <SkeletonArticle/>
+                                </> 
+
+                        }
+
+                    </div>
+                </div>
+                
+                <Footer/>
+            </motion.div>
+        </> 
+        
+    );
+}
