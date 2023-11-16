87a396a1dffae13b96502f560c5bab3a5c3590c5
vandilsonbrito <vandilson25@outlook.com>
2023-11-16 15:16:56 -0300

    Add PrismicProvider, GlobalPRovider and HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import { PrismicProvider } from '@prismicio/react';
+import { client } from './services/prismic.ts';
+import { GlobalProvider } from './components/GlobalProviderMenuIsOpen/MenuIsOpen.jsx';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <PrismicProvider client={client}>
+        <GlobalProvider>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </GlobalProvider>
+      </PrismicProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
