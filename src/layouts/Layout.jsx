aeb7978a8155cf919714e5ca3c2c195337ee47fe
vandilsonbrito <vandilson25@outlook.com>
2023-11-16 15:16:22 -0300

    Add ArticleCTASection and Gallery

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -6,6 +6,8 @@ import Sobre from '../components/AboutSection/AboutSection';
 import Canal from '../components/ChannelSection/ChannelSection';
 import Contato from '../components/ContactSection/ContactSection';
 import Footer from "../components/Footer/Footer";
+import Gallery from "../components/GallerySection/Gallery";
+import ArticleCTASection from "../components/ArticleCTASection/ArticleCTASection";
 
 
 export default function Layout() {
@@ -15,7 +17,9 @@ export default function Layout() {
         <Banner/>
         <Convencimento/>
         <Sobre/>
+        <ArticleCTASection/>
         <Canal/>
+        <Gallery/>
         <Contato/>
         <Footer/>
     </div>
