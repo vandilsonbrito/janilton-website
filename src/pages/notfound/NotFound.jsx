347c00b1bb1f6fd88e23bb2124ff9cb8e11a9cc4
vandilsonbrito <vandilson25@outlook.com>
2023-11-20 12:16:51 -0300

    Create Not Found page

---
+++ src/pages/notfound/NotFound.jsx
@@ -0,0 +1,20 @@
+import HeaderOutter from '../../components/Header/HeaderOutter';
+import Footer from '../../components/Footer/Footer';
+import { BiSolidErrorAlt } from "react-icons/bi";
+
+
+export default function NotFound() {
+
+    return (
+        <>
+            <HeaderOutter/>
+            <div className='w-full h-full min-h-screen bg-white text-black font-Montserrat pt-20 flex flex-col justify-center items-center gap-5'>
+                <BiSolidErrorAlt className='text-6xl lg:text-8xl'/>
+                <p className='text-3xl xl:text-6xl'>Error 404</p>
+                <p className='text-2xl xl:text-4xl'>Página Não encontrada.</p>
+
+            </div>
+            <Footer/>
+        </>
+    );
+}
