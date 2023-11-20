08caa67b4a3ea2906e16ed50ffae6f764c97ec04
vandilsonbrito <vandilson25@outlook.com>
2023-11-20 12:19:25 -0300

    Add images alt and update styles

--- src/pages/Artigos/MenuArtigos/MenuArtigos.jsx
+++ src/pages/Artigos/MenuArtigos/MenuArtigos.jsx
@@ -13,14 +13,13 @@ export default function MenuArtigos() {
         document.body.style.overflow = 'auto';
       }, []);
 
-    const { titles, descriptions, textContents, imagesurl, slugs } = handleDocument();
+    const { titles, descriptions, textContents, imagesurl, imagesalt, slugs } = handleDocument();
     
-    const showArticles = (titles, descriptions, textContents, imagesurl, slugs) => {
+    const showArticles = (titles, descriptions, textContents, imagesurl, imagesalt, slugs) => {
         const artigosDisplays = [];
         for (let i = 0; i < titles.length; i++) {
-            artigosDisplays.push(<DisplayArtigos key={i} title={titles[i]} description={descriptions[i]} textContent={textContents[i]} imageurl={imagesurl[i]} slug={slugs[i]} />);
+            artigosDisplays.push(<DisplayArtigos key={i} title={titles[i]} description={descriptions[i]} textContent={textContents[i]} imageurl={imagesurl[i]} imagealt={imagesalt[i]} slug={slugs[i]} />);
         }
-        console.log('Menu de Artigos carregados!')
         return artigosDisplays;
     }
 
@@ -39,17 +38,17 @@ export default function MenuArtigos() {
                 animate={'visible'}
                 variants={fadeInOut}
                 transition={{ duration: 0.3 }}
-                className='w-full h-[50%]'
+                className='w-full min-h-screen'
                 >
                     
                 {<HeaderOutter/>}
-                <div className='w-full h-full bg-white text-white py-28 lg:py-40 flex flex-col justify-center'>
+                <div className='w-full h-full min-h-screen   bg-white text-white py-28 lg:py-40 flex flex-col justify-center'>
                     <div className="w-full flex justify-center pb-10 md:pb-20">
                         <h1 className='text-3xl md:text-4xl text-black uppercase font-semibold'>Artigos</h1>
                     </div>
                     <div className="w-full h-full flex flex-wrap justify-center items-center gap-8 xl:gap-14">
-                        { (titles && descriptions && textContents && imagesurl && slugs) ? 
-                            showArticles(titles, descriptions, textContents, imagesurl, slugs) 
+                        { (titles && descriptions && textContents && imagesurl && imagesalt  && slugs) ? 
+                            showArticles(titles, descriptions, textContents, imagesurl, imagesalt, slugs) 
                             :
                                 <>
                                     <SkeletonArticle/>
