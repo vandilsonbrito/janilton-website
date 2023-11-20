18b9757e8780833686d444c9c68c4db26102badd
vandilsonbrito <vandilson25@outlook.com>
2023-11-20 12:17:40 -0300

    Update Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Sobre from './components/AboutSection/AboutSection';
-import DisplayArtigos from './pages/Artigos/MenuArtigos/MenuArtigos';
-import Artigos from './pages/Artigos/Artigos/Artigos';
+import DisplayArtigos from './pages/artigos/menuArtigos/MenuArtigos';
+import Artigos from './pages/artigos/artigos/Artigos';
 import MaisSobre from './pages/Sobre/Sobre';
 import CanalSection from './components/ChannelSection/ChannelSection';
 import ContactSection from './components/ContactSection/ContactSection';
 import SobrePage from './pages/Sobre/Sobre';
+import NotFound from './pages/notfound/NotFound';
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
           <Route path='/Sobre' element={<Sobre/>}/>
           <Route path='/Canal' element={<CanalSection/>}/>
           <Route path='/Contato' element={<ContactSection/>}/>
+          <Route path='/*' element={<NotFound/>}/>
         </Routes>   
     </>
   )
