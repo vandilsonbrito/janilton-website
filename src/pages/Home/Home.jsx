320752374017590d3b536d8bc6f97da35c1a1637
vandilsonbrito <vandilson25@outlook.com>
2023-11-24 16:33:27 -0300

    Update title

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
         <Helmet>
+          <title>Professor Janilton Almeida. Aprenda a dar aulas criativas.</title>
           <meta name="description" content="O Professor Janilton é um especialista na arte de dar aulas criativas, pesquisa educacional e inovação. É um professor de Geografia com mais de vinte e cinco anos de atuação com Mestrado em Ensino de Ciências e Doutorado em Ciências. Ele possui um canal no YouTube onde compartilha seu conhecimento para ajudar alunos (com conteúdos de Geografia) e professores (para ajudá-los na elaboração de aulas criativas). Além disso, ele escreve artigos de opinião sobre diversos temas da área acadêmica"/>
         </Helmet>
         <Layout/>
